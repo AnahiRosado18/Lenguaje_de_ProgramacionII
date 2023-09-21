345647b76bb5f70c282efdbda316de59e61543cf
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2023-09-21 12:50:25 -0600

    Add files via upload

---
+++ Tareas/Practica3/AutomovilFord.java
@@ -0,0 +1,91 @@
+/**
+* Automovil.java
+*
+* @author H�ctor Quej Cosgaya
+* @author Jos� Aguilar Canepa
+*
+* @created 05/11/12 10:50 PM
+*
+* Esta clase modela exclusivamente automoviles de la marca "Ford".
+*/
+public class AutomovilFord {
+	
+	int velocidad;						// La velocidad a la que se desplaza el auto
+	String color;						// El color del auto
+	public String modelo;					// El modelo del auto
+	Motor motor;						// El motor del auto
+	public static final String marca = "Ford";	// La marca del auto
+		
+	/**
+	* Inicializa un nuevo objeto Automovil.
+	* 
+	* @param modelo: El modelo del auto.
+	* @param color: El color del auto
+	*/
+	public AutomovilFord(String modelo, String color){
+		this.modelo = modelo;
+		this.color = color;
+		velocidad = 0;
+		motor = new Motor();
+	}
+	
+	/**
+	* Inicializa un nuevo objeto Automovil.
+	*/
+	public AutomovilFord(){
+		this("EcoSport", "Azul");
+	}
+	
+	/**
+	* Enciende el motor del automovil.
+	*/
+	public void encenderMotor() {
+		motor.encendido = true;
+		System.out.println("Motor encendido");
+	}
+	
+	/**
+	* Apaga el motor del automovil.
+	*/
+	public void apagarMotor() {
+		motor.encendido = false;
+		System.out.println("Motor apagado");
+	}
+	
+	/**
+	* Incrementa la velocidad del automovil en la cantidad de kil�metros/hora especificada.
+	*
+	* @param cantidad: El n�mero de km/h en que aumentar� la velocidad del auto.
+	*/
+	public void acelerar() {
+		if (motor.encendido) {
+			motor.revolucionar();
+			velocidad = (motor.revoluciones / 100);
+			System.out.println("Vamos a " + velocidad + " kph");			
+		} else {
+			System.out.println("El motor est� apagado!");
+		}
+	}
+	
+	/**
+	* Reduce a cero la velocidad del auto.
+	*/
+	public void frenar(int getRevoluciones) {
+		
+		if(velocidad < 0) velocidad = 0;
+		System.out.println("Vamos a " + velocidad + " kph");	
+
+
+	}
+	
+	/**
+	* Nos muestra la velocidad a la que se est� desplazando el auto.
+	* 
+	* @return la velocidad del auto.
+	*/
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	
+}
