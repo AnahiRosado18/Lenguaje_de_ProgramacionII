6584fc34e69ce82cccbb9f813dd5321ddb1efba1
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2023-09-22 10:08:04 -0600

    Add files via upload

---
+++ Tareas/Practica3/semestre3/AlumnoFinal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semestre3;
+
+/**
+ *
+ * @author lunar
+ */
+public class AlumnoFinal1 {
+    
+    public static void main (String[] args){
+        
+        Telefono telefonoTutor = new Telefono("+52","981","9811693047");
+        Tutor tutor01 = new Tutor("Arlethe", 50,telefonoTutor);
+        Alumno Alumno01 = new Alumno("Jorge","Diseño Multimedia", 469142,3, 'C',tutor01); 
+        Alumno Alumno1 = Alumno01;
+        Tutor tutor1=tutor01;
+        System.out.println("Los datos del alumno son: "+Alumno1);
+        System.out.println("El telefono del Tutor es: "+Alumno01.getTutor().getTelefono().toString());
+       
+    }
+}
