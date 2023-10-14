8d31eddcaa56d07caf7701981235bd003a9ca0a6
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2023-10-13 20:19:40 -0600

    Add files via upload

---
+++ Pokemon_Abstraccion/pokemon/PokemonHada.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemon;
+
+/**
+ *
+ * @author lunar
+ */
+public class PokemonHada extends Pokemon {
+    
+    public PokemonHada(String nombre, int nivel){
+        super(nombre,Tipo.HADA, nivel);
+    
+        ListaMovimientos listaMovimientos = new ListaMovimientos();
+    
+        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Imagen"));
+        setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Cabezazo Zen"));
+        setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Ladrón"));
+        setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Sombra Vil"));
+    }
+    
+    @Override
+    public double obtenerEfectividad(Pokemon pokemon){
+        double efectividad= 1.0;
+        
+        if(pokemon.getTipo()== Tipo.LUCHA) efectividad = 2.0;
+        if(pokemon.getTipo()== Tipo.DRAGON) efectividad = 2.0;
+        if(pokemon.getTipo()== Tipo.SINIESTRO) efectividad = 2.0;
+        if(pokemon.getTipo()== Tipo.ACERO) efectividad = 0.0;
+        if(pokemon.getTipo()== Tipo.FUEGO) efectividad = 1.0;
+        if(pokemon.getTipo()== Tipo.VENENO) efectividad = 0.0;
+        return efectividad;
+    }
+}
+
