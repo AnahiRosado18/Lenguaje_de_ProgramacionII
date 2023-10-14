d5f892657c0c343115d6d537a35e76d9f5a68ca9
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2023-10-13 21:33:55 -0600

    Update PokemonAgua.java

--- Pokemon_Abstraccion/pokemon/PokemonAgua.java
+++ Pokemon_Abstraccion/pokemon/PokemonAgua.java
@@ -29,9 +29,9 @@ public class PokemonAgua extends Pokemon { //Como tal esta es la herencia por lo
         if(pokemon.getTipo()== Tipo.TIERRA) efectividad = 2.0;
         if(pokemon.getTipo()== Tipo.FUEGO) efectividad = 2.0;
         if(pokemon.getTipo()== Tipo.ROCA) efectividad = 2.0;
-        if(pokemon.getTipo()== Tipo.PLANTA) efectividad = 1.0;
-        if(pokemon.getTipo()== Tipo.DRAGON) efectividad = 1.0;
-        if(pokemon.getTipo()== Tipo.AGUA) efectividad = 1.0;
+        if(pokemon.getTipo()== Tipo.PLANTA) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.DRAGON) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.AGUA) efectividad = 0.5;
         if(pokemon.getTipo()== Tipo.ELECTRICO) efectividad = 0.0;
         return efectividad;
     }
