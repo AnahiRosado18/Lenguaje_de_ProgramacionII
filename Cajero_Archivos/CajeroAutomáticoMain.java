d8abd8202da7cddb372634ae158613427e96a0f6
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2023-11-15 17:49:15 -0600

    Update CajeroAutomáticoMain.java

--- Cajero_Archivos/CajeroAutomáticoMain.java
+++ Cajero_Archivos/CajeroAutomáticoMain.java
@@ -14,7 +14,7 @@ public class CajeroAutomáticoMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         CajeroAutomatico cajero = new CajeroAutomatico();
+        CajeroAutomatico cajero = new CajeroAutomatico();
         cajero.iniciarSesion();
 
     }
